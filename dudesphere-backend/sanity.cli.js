45e8febf00c89802ef8233da4778fa62d27d02da
Abdelrahman Elrefai <team.man00@gmail.com>
2024-02-09 10:53:14 +0200

    Main Setup Base Is Now Ready..

---
+++ dudesphere-backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '40e9rs4v',
+    dataset: 'production'
+  }
+})
---
+++ dudesphere-frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <h1 className='text-3xl text-white py-4 text-center font-extrabold bg-gradient-to-r from-purple-700 to-sky-700'>Hello, World!</h1>
+    )
+}
+
+export default App;
